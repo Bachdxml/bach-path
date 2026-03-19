3f0b574270f3742b49d64b665071f5daea2ba560
bachdxai <lab@bachdx.com>
2026-03-19 14:29:35 -0700

    Add CombinedLoss import to __init__.py

--- wsi-fungal-segmentation/src/__init__.py
+++ wsi-fungal-segmentation/src/__init__.py
@@ -1,4 +1,4 @@
 from .dataset import AugmentedWSI_Dataset, TilePair, WSI_Dataset, WSIDatasetIndex
-from .losses import AsymmetricSimilarityLoss
+from .losses import AsymmetricSimilarityLoss, CombinedLoss
 from .metrics import compute_all_metrics
 from .model import ResidualAttentionUNet
